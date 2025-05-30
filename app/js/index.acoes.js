b30adcd60c49fb48633a4419e40873c99353253f
RobsonMourasz <robsonic10@gmail.com>
2025-05-30 12:03:11 -0300

    Adiciona estilo de carregamento com animação e mensagem "Carregando..."

---
+++ app/js/index.acoes.js
@@ -0,0 +1,51 @@
+(()=>{
+    document.addEventListener('DOMContentLoaded', () => {
+        document.getElementById("sobre").classList.remove("d-none")
+        document.getElementById("Titulo").innerText = "Sobre Mim";
+        setTimeout(() => {
+            document.getElementById("carregando").classList.remove("carregando");
+        }),1000;
+        
+    });
+
+    document.querySelectorAll("a").forEach((link) => {
+
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            document.querySelectorAll("a").forEach((link) => {
+
+                if (link.getAttribute("targ") == "menu"){
+                    document.querySelector(link.getAttribute("href")).classList.add("d-none");
+                    link.classList.remove("link-active");
+                    
+                }
+            });
+
+            const target = link.getAttribute("href").replace("#", "");
+
+            if (target) {
+                
+                if (target === "sobre"){
+                    document.getElementById("Titulo").innerText = "Sobre Mim";
+                    document.getElementById("sobre").classList.remove("d-none");
+                    document.getElementById("sobre").classList.add("link-active");
+                }
+                else if (target === "habilidades"){
+                    document.getElementById("Titulo").innerText = "Minhas Habilidades";
+                } 
+                else if (target === "projetos"){
+                    document.getElementById("Titulo").innerText = "Meus Projetos";
+                }
+                else if (target === "contato"){
+                    console.log("Sobre Mim");
+                    document.getElementById("Titulo").innerText = "Entre em contato";
+                }
+
+                document.getElementById(target).classList.remove("d-none");
+            }
+
+        });
+    });
+
+})()
