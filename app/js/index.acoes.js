1d010cb1092b90dacad68dcebd8de95be44ef0bf
RobsonMourasz <robsonic10@gmail.com>
2025-06-02 10:07:35 -0300

    Refatora código JavaScript para melhorar a legibilidade e a estrutura do evento de carregamento

--- app/js/index.acoes.js
+++ app/js/index.acoes.js
@@ -1,11 +1,11 @@
-(()=>{
+(() => {
     document.addEventListener('DOMContentLoaded', () => {
         document.getElementById("sobre").classList.remove("d-none")
         document.getElementById("Titulo").innerText = "Sobre Mim";
         setTimeout(() => {
             document.getElementById("carregando").classList.remove("carregando");
-        }),1000;
-        
+        }), 1000;
+
     });
 
     document.querySelectorAll("a").forEach((link) => {
@@ -15,30 +15,28 @@
 
             document.querySelectorAll("a").forEach((link) => {
 
-                if (link.getAttribute("targ") == "menu"){
+                if (link.getAttribute("targ") == "menu") {
                     document.querySelector(link.getAttribute("href")).classList.add("d-none");
                     link.classList.remove("link-active");
-                    
+
                 }
             });
 
+            link.classList.add("link-active");
             const target = link.getAttribute("href").replace("#", "");
 
             if (target) {
-                
-                if (target === "sobre"){
+                console.log("Target: ", target);
+                if (target === "sobre") {
                     document.getElementById("Titulo").innerText = "Sobre Mim";
-                    document.getElementById("sobre").classList.remove("d-none");
-                    document.getElementById("sobre").classList.add("link-active");
                 }
-                else if (target === "habilidades"){
+                else if (target === "habilidades") {
                     document.getElementById("Titulo").innerText = "Minhas Habilidades";
-                } 
-                else if (target === "projetos"){
+                }
+                else if (target === "projetos") {
                     document.getElementById("Titulo").innerText = "Meus Projetos";
                 }
-                else if (target === "contato"){
-                    console.log("Sobre Mim");
+                else if (target === "contato") {
                     document.getElementById("Titulo").innerText = "Entre em contato";
                 }
 
